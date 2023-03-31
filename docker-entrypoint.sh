57afea45b983b46e6ed4e89369256d68b385298f
Tony Smith <tsmith@twilio.com>
2023-03-31 11:28:42 +0100

    Update docker environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,10 @@ cd $(dirname $0)
 
 [ -d build ] && rm -rf build
 
-./deploy.sh --genkeys --log
+if [[ -n "${MICROVISOR_GITHUB_ACTION_FLOW}" ]]; then
+  # Build only
+  twilio microvisor:deploy . -b
+else
+  # Build and deploy -- requires env vars for device SID and Twilio creds to be set
+  twilio microvisor:deploy . --devicesid ${MV_DEVICE_SID} --genkeys --log
+fi
